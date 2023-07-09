2c67ea98ed6ee82426163707ad91b5de3546bfde
Githonil <anthony.gimenez06@gmail.com>
2023-07-09 22:44:25 +0200

    Ajout des évènements posé/cassé un block

--- app/main/java/com/githonil/fallenkingdom/FallenKingdom.java
+++ app/main/java/com/githonil/fallenkingdom/FallenKingdom.java
@@ -1,18 +1,46 @@
-/*package com.githonil.fallenkingdom;
+package com.githonil.fallenkingdom;
+
+import com.githonil.fallenkingdom.teams.TeamInterface;
+import com.githonil.fallenkingdom.claims.ClaimInterface;
+
+import com.githonil.fallenkingdom.listeners.blocks.BlockBreakListener;
+import com.githonil.fallenkingdom.listeners.blocks.BlockPlaceListener;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.UUID;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * This class is the main class of the plugin.
  */
-/*public class FallenKingdom extends JavaPlugin {
+public class FallenKingdom extends JavaPlugin {
+
+    /**
+     * This attribute represents a player with his team.
+     */
+    private HashMap<UUID, TeamInterface> teammatesMap;
+
+
+
+    /**
+     * This attribute represents all the claims.
+     */
+    private Set<ClaimInterface> claims;
+
+
 
     /**
      * This method is activated when the plugin is loaded.
      */
-    /*@Override
+    @Override
     public void onLoad() {
-        
+        teammatesMap = new HashMap<>();
+        claims = new Set<>();
+
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(claims), this);
+        getServer().getPluginManager().registerEvents(new BlockPlaceListener(claims), this);
     }
 
 
@@ -20,9 +48,9 @@ import org.bukkit.plugin.java.JavaPlugin;
     /**
      * This method is activated when the plugin is enabled.
      */
-    /*@Override
+    @Override
     public void onEnable() {
         System.out.println("The fallen kingdom is ready !");
     }
 
-}*/
+}
